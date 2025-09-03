fc356acfebf20c70ad4f2e9093cbfb804cc837ac
Hoang Doan An <an.hoang@asnet.com.vn>
2025-09-03 11:00:33 +0700

    feat: update styles in article portable content

--- src/schema-types/custom-fields/portable-content.ts
+++ src/schema-types/custom-fields/portable-content.ts
@@ -10,6 +10,15 @@ const portableContent = defineType({
   of: [
     {
       type: 'block',
+      styles: [
+        { title: 'Normal', value: 'normal' },
+        { title: 'Heading 2', value: 'h2' },
+        { title: 'Heading 3', value: 'h3' },
+        { title: 'Heading 4', value: 'h4' },
+        { title: 'Heading 5', value: 'h5' },
+        { title: 'Heading 6', value: 'h6' },
+        { title: 'Quote', value: 'blockquote' },
+      ],
       marks: {
         decorators: [
           { title: 'Strong', value: 'strong' },
