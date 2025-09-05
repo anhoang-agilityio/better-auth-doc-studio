af97b1b14611f81f929419257f385ba200877e6e
Hoang Doan An <an.hoang@asnet.com.vn>
2025-09-05 10:45:28 +0700

    feat: update CLI configuration to use environment variables for project ID and dataset

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,12 @@
 import { defineCliConfig } from 'sanity/cli';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { env } from '@/config/env';
+
 export default defineCliConfig({
   api: {
-    projectId: '0gl88wwy',
-    dataset: 'production',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
   },
   /**
    * Enable auto-updates for studios.
